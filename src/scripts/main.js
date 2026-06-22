f0bb1e473f90780067806ae4f78a466d9c957717
Elvira Petrenko <rukola2008_r@users.noreply.github.com>
2026-06-22 17:22:43 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const burger = document.querySelector('.header__burger');
+const menu = document.querySelector('#menu');
+const closeBtn = document.querySelector('.menu__close');
+
+burger.addEventListener('click', () => {
+  menu.classList.add('is-open');
+});
+
+closeBtn.addEventListener('click', () => {
+  menu.classList.remove('is-open');
+});
